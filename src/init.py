6d0c7a7aa6fb2dbdb182bfe4a053f147d9a22acf
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-10-19 20:29:40 +0200

    refactor: Use more consistent names

--- src/init.py
+++ src/init.py
@@ -13,12 +13,11 @@ import matplotlib.pyplot as plt  # for plotting
 """
 Import your input data for the model
 """
-group = 12
+group = 10
 season = 'summer'
 
 filename = f'../input/AssB_Input_Group{group}_{season}.csv'
-data = pd.read_csv(
-    filename, parse_dates=True)
+data = pd.read_csv(filename, parse_dates=True)
 data.columns = ['start', 'end', 'demand', 'pv_gen', 'price', 'emission_factor']
 
 
@@ -30,8 +29,8 @@ Delta_t = 0.25  # 15 minute (0.25 hour) intervals
 T = int(24*3*1/Delta_t)  # number of time-slots (in three days)
 
 # Limits on grid and max, min, and initial SOC
-Pgridmax = 3.0  # [kW]
-Pbatmax = 4.0  # [kW]
+P_grid_max = 3.0  # [kW]
+P_bat_max = 4.0  # [kW]
 
 SoC_min = 0.2  # [-] (battery min state of charge)
 SoC_max = 1  # [-] (battery max state of charge)
@@ -56,11 +55,10 @@ model = gp.Model('Optimising scambot 2000')
 """
 Step 2: Define variables
 """
-grid_power = model.addVars(data.index, lb=-Pgridmax, ub=Pgridmax)
-battery_charge = model.addVars(
-    data.index, lb=SoC_min * C_bat, ub=SoC_max * C_bat)
-battery_power_in = model.addVars(data.index, ub=Pbatmax)
-battery_power_out = model.addVars(data.index, ub=Pbatmax)
+P_grid = model.addVars(data.index, lb=-P_grid_max, ub=P_grid_max)
+SoC = model.addVars(data.index, lb=SoC_min * C_bat, ub=SoC_max * C_bat)
+P_bat_in = model.addVars(data.index, ub=P_bat_max)
+P_bat_out = model.addVars(data.index, ub=P_bat_max)
 
 
 """
@@ -68,24 +66,24 @@ Step 3: Add constraints
 """
 # Power boundaries
 # Power balance formula
-model.addConstrs(grid_power[t] == data.demand[t]
-                 - data.pv_gen[t] - battery_power_out[t]
-                 + battery_power_in[t] for t in range(T))
+model.addConstrs(P_grid[t] == data.demand[t] - data.pv_gen[t] -
+                 P_bat_out[t] + P_bat_in[t] for t in range(T))
 
 # Battery SoC dynamics constraint
-model.addConstrs(battery_charge[t] == battery_charge[t - 1] + (battery_power_in[t] *
-                 Delta_t * eff_ch) - (battery_power_out[t] * Delta_t) / eff_dis for t in range(1, T))
+model.addConstrs(SoC[t] == SoC[t - 1] + (P_bat_in[t] * Delta_t * eff_ch)
+                 - (P_bat_out[t] * Delta_t) / eff_dis for t in range(1, T))
 
-model.addConstr(battery_power_in[0] == (
-    battery_charge[0]-SoC0*C_bat)/(Delta_t*eff_ch))
-model.addConstr(battery_power_out[0] == (
-    (SoC0*C_bat-battery_charge[0])*eff_dis)/Delta_t)
+model.addConstr(SoC[0] == SoC0 * C_bat + (P_bat_in[0] * Delta_t * eff_ch)
+                - (P_bat_out[0] * Delta_t) / eff_dis)
+
+model.addConstr(P_bat_in[0] == (SoC[0] - SoC0 * C_bat) / (Delta_t * eff_ch))
+model.addConstr(P_bat_out[0] == ((SoC0 * C_bat - SoC[0]) * eff_dis) / Delta_t)
 
 
 """
 Step 4: Set objective function
 """
-obj = gp.quicksum(data.price[t]*grid_power[t]*Delta_t for t in range(T))
+obj = gp.quicksum(data.price[t]*P_grid[t]*Delta_t for t in range(T))
 
 
 """
@@ -98,10 +96,10 @@ if model.status == gp.GRB.OPTIMAL:
     """
     Step 6: Print variables values for optimal solution
     """
-    data['grid'] = model.getAttr('x', grid_power).values()
-    data['battery_charge'] = model.getAttr('x', battery_charge).values()
-    data['battery_in'] = model.getAttr('x', battery_power_in).values()
-    data['battery_out'] = model.getAttr('x', battery_power_out).values()
+    data['grid'] = model.getAttr('x', P_grid).values()
+    data['battery_charge'] = model.getAttr('x', SoC).values()
+    data['battery_in'] = model.getAttr('x', P_bat_in).values()
+    data['battery_out'] = model.getAttr('x', P_bat_out).values()
 
     """
     Step 7: Plot optimal power output from each generator

391ae8a9ab92519fa189b3bbfa53ff4f94cd5978
raj0974722 <r.kumar@students.uu.nl>
2021-10-21 16:45:33 +0200

    computing for each season
    using for-loop for winter and summer.

--- src/question2.py
+++ src/question2.py
@@ -14,26 +14,28 @@ def plot_data(data, filename, labels):
     ax.legend(labels=labels)
     plots.savefig(f'../output/{filename}')
 
+def optimum_perseason(season):
+    filename = f'../input/AssB_Input_Group{group}_{season}.csv'
+    data = pd.read_csv(filename, parse_dates=True)
+    data.columns = ['start', 'end', 'demand', 'pv_gen', 'price', 'emission_factor']
+    
+    # Run the model
+    results = mf.run(data)
+    
+    # Plot the results input data
+    data = data[['demand', 'pv_gen']]
+    filename = f'demand_and_pv_{season}_{group}.png'
+    legend = ['Demand', 'PV']
+    plot_data(data, filename, legend)
+    
+    # Plot the results
+    data = results[['demand', 'pv_gen', 'grid', 'battery_power']]
+    filename = f'results_{season}_{group}.png'
+    legend = ['Demand', 'PV', 'Grid', 'Battery']
+    plot_data(data, filename, legend)
 
 # Import the data
 group = 12
-season = 'summer'
-
-filename = f'../input/AssB_Input_Group{group}_{season}.csv'
-data = pd.read_csv(filename, parse_dates=True)
-data.columns = ['start', 'end', 'demand', 'pv_gen', 'price', 'emission_factor']
-
-# Run the model
-results = mf.run(data)
-
-# Plot the results input data
-data = data[['demand', 'pv_gen']]
-filename = f'demand_and_pv_{season}_{group}.png'
-legend = ['Demand', 'PV']
-plot_data(data, filename, legend)
-
-# Plot the results
-data = results[['demand', 'pv_gen', 'grid', 'battery_power']]
-filename = f'results_{season}_{group}.png'
-legend = ['Demand', 'PV', 'Grid', 'Battery']
-plot_data(data, filename, legend)
+seasons = ["summer", "winter"]
+for season in seasons:
+    optimum_perseason(season)

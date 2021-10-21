26798315b8a7d89d6c6f5c91c100867fa2f0875e
Erikvbattum <e.j.d.vanbattum@students.uu.nl>
2021-10-21 15:57:25 +0200

    made it so that seasons change automatically
    put everything in a function that runs twice to doe both seasons

--- src/question3.py
+++ src/question3.py
@@ -21,34 +21,40 @@ def plot_data(data, filename, labels):
 
 # Import the data
 group = 12
-season = 'summer'
+season = ["summer", "winter"]
 
-filename = f'../input/AssB_Input_Group{group}_{season}.csv'
-data = pd.read_csv(filename, parse_dates=True)
-data.columns = ['start', 'end', 'demand', 'pv_gen', 'price', 'emission_factor']
+def season_changer(season):
 
-# Run the model for cost optimization
-cost_results = mf.run(data, 'emissions')
-min_emissions = (cost_results.grid * cost_results.emission_factor * 0.25).sum()
+    filename = f'../input/AssB_Input_Group{group}_{season}.csv'
+    data = pd.read_csv(filename, parse_dates=True)
+    data.columns = ['start', 'end', 'demand', 'pv_gen', 'price', 'emission_factor']
+    
+    # Run the model for cost optimization
+    cost_results = mf.run(data, 'emissions')
+    min_emissions = (cost_results.grid * cost_results.emission_factor * 0.25).sum()
+    
+    # Run the model for emissions optimization
+    em_results = mf.run(data, 'cost')
+    max_emissions = (em_results.grid * em_results.emission_factor * 0.25).sum()
+    
+    # Calculate the bins for the Pareto frontier
+    no_bins = 10
+    bin_width = (max_emissions - min_emissions) / (no_bins - 1)
+    emissions_bins = np.arange(min_emissions, max_emissions + bin_width, bin_width)
+    
+    # Create the Pareto frontier
+    pareto_dataframe = pd.DataFrame(columns=['cost', 'emissions'])
+    
+    for index, max_emissions in enumerate(emissions_bins):
+        print('\rRunning optimization {} of {}'.format(
+            index + 1, len(emissions_bins)), end='')
+        results = mf.run(data, 'cost', max_emissions)
+        cost = (results.grid * results.price * 0.25).sum()
+        emission = (results.grid * results.emission_factor * 0.25).sum()
+        pareto_dataframe.loc[max_emissions] = [cost, emission]
+    
+    print('\r Succesfully created the Pareto frontier')
+    pareto_dataframe.plot(kind='scatter', x='emissions', y='cost')
 
-# Run the model for emissions optimization
-em_results = mf.run(data, 'cost')
-max_emissions = (em_results.grid * em_results.emission_factor * 0.25).sum()
-
-# Calculate the bins for the Pareto frontier
-no_bins = 10
-bin_width = (max_emissions - min_emissions) / (no_bins - 1)
-emissions_bins = np.arange(min_emissions, max_emissions + bin_width, bin_width)
-
-# Create the Pareto frontier
-pareto_dataframe = pd.DataFrame(columns=['cost', 'emissions'])
-for index, max_emissions in enumerate(emissions_bins):
-    print('\rRunning optimization {} of {}'.format(
-        index + 1, len(emissions_bins)), end='')
-    results = mf.run(data, 'cost', max_emissions)
-    cost = (results.grid * results.price * 0.25).sum()
-    emission = (results.grid * results.emission_factor * 0.25).sum()
-    pareto_dataframe.loc[max_emissions] = [cost, emission]
-
-print('\r Succesfully created the Pareto frontier')
-pareto_dataframe.plot(kind='scatter', x='emissions', y='cost')
+for s in season:
+    season_changer(s)

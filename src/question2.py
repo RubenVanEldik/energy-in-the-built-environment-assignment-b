4eb3ba9b4a73126fe0a657dafd18f1ef2149fb5c
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2021-10-21 20:36:34 +0200

    feat: Add a power flows plot for summer and winter

--- src/question2.py
+++ src/question2.py
@@ -6,36 +6,32 @@ import plots
 plt.rcParams['font.family'] = 'serif'
 
 
-def plot_data(data, filename, labels):
-    ax = data.plot(color=['#aa3026', '#91723c', '#85ab7b', '#915a8d'])
-    ax.set_xlabel('Time [hour]')
-    ax.set_ylabel('Power [kW]')
-    ax.legend(labels=labels)
-    plots.savefig(f'../output/{filename}')
+# Set the group and seasons
+group = 12
+seasons = ['Summer', 'Winter']
+
+# Create two figures
+xlabel = 'Time [hour]'
+ylabel = 'Power [kW]'
+figure, axes = plots.create_plot_with_subplots(
+    2, 1, xlabel=xlabel, ylabel=ylabel)
 
 
-def optimum_perseason(season):
-    # Run the model
+# Create a subplot for each season
+for index, season in enumerate(seasons):
+    # Create the Pareto frontier for both the line and scatter plot
     results = mf.run(group, season, 'emissions')
 
     cost = (results.grid * results.price * 0.25).sum()
     print('Optimised cost for {} season is {}'.format(season, cost))
 
-    # Plot the results input data
-    data = results[['demand', 'pv_gen']]
-    filename = f'demand_and_pv_{season}_{group}.png'
-    legend = ['Demand', 'PV']
-    plot_data(data, filename, legend)
+    # Create the subplot
+    subplot = axes[index]
+    subplot.title.set_text(season)
+    subplot.plot(results.demand, color='#aa3026')
+    subplot.plot(results.pv_gen, color='#91723c')
+    subplot.plot(results.grid, color='#85ab7b')
+    subplot.plot(results.battery_power, color='#915a8d')
 
-    # Plot the results
-    data = results[['demand', 'pv_gen', 'grid', 'battery_power']]
-    filename = f'results_{season}_{group}.png'
-    legend = ['Demand', 'PV', 'Grid', 'Battery']
-    plot_data(data, filename, legend)
-
-
-# Import the data
-group = 12
-seasons = ["summer", "winter"]
-for season in seasons:
-    optimum_perseason(season)
+# Save the figure
+plots.savefig('../output/power_flows.png')
